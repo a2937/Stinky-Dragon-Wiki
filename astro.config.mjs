782b3e5f05d5a9e3022760f9131056f4903f655b
Anna <a.rcottrill521@gmail.com>
2025-08-26 10:18:59 -0400

    chore: add basic tooling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://a2937.github.io',
-  base: '/Stinky-Dragon-Wiki',
+  site: "https://a2937.github.io",
+  base: "/Stinky-Dragon-Wiki",
 });
